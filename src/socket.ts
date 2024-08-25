9d855b6cef47142091421453ebf0f8e528411da8
Jessica Gondim <jessicagbsg@gmail.com>
2024-08-25 00:55:24 -0300

    feat: socket messages to users

--- src/socket.ts
+++ src/socket.ts
@@ -5,6 +5,7 @@ import { AuthenticationService } from "./services/auth.service";
 import { NoteService } from "./services/note.service";
 import { NoteRepository } from "./database/repositories/note.repository";
 import { UpdateNoteDTO } from "./database/models/notes";
+import { capitalize } from "lodash";
 
 const userRepository = new UserRepository();
 const authenticationService = new AuthenticationService({ userRepository });
@@ -35,6 +36,8 @@ export const setUpSocket = (
   nsp.on("connection", (socket) => {
     console.log("New client connected");
 
+    const userRooms = new Map();
+
     socket.on("join-room", async (content: { room: string }) => {
       const user = await authenticationService.getUserFromToken(
         socket.handshake.auth.token || socket.handshake.headers.access_token?.toString()
@@ -44,13 +47,53 @@ export const setUpSocket = (
       const note = await noteService.findByRoom(content.room, user.id);
       if (!note) return nsp.to(note.room).emit("join-room", "Room not found");
 
+      const previousRooms = userRooms.get(user.id) || [];
+      for (const prevRoom of previousRooms) {
+        if (prevRoom !== content.room) {
+          socket.leave(prevRoom);
+          nsp.to(prevRoom).emit("leave-room", {
+            user: user.id,
+            message: `${capitalize(user.firstName)} ${capitalize(
+              user.lastName
+            )} left collaboration`,
+          });
+        }
+      }
+
+      userRooms.set(user.id, [content.room]);
+
       if (note.ownerId !== user.id && !note.members.includes(user.id)) {
         note.members.push(user.id);
         await noteService.update(note.id, { members: note.members });
       }
 
       socket.join(content.room);
-      nsp.to(note.room).emit("join-room", "Joined room successfully");
+      nsp.to(content.room).emit("join-room", {
+        user: user.id,
+        message: `${capitalize(user.firstName)} ${capitalize(user.lastName)} joined collaboration`,
+      });
+    });
+
+    socket.on("leave-room", async (content: { room: string }) => {
+      const user = await authenticationService.getUserFromToken(
+        socket.handshake.auth.token || socket.handshake.headers.access_token?.toString()
+      );
+      if (!user) return;
+
+      const note = await noteService.findByRoom(content.room, user.id);
+      if (!note) return nsp.to(content.room).emit("leave-room", "Room not found");
+
+      const userRoomList = userRooms.get(user.id) || [];
+      const updatedRoomList = userRoomList.filter((room: string) => room !== content.room);
+      userRooms.set(user.id, updatedRoomList);
+
+      socket.leave(content.room);
+      nsp
+        .to(content.room)
+        .emit("leave-room", {
+          user: user.id,
+          message: `${capitalize(user.firstName)} ${capitalize(user.lastName)} left collaboration`,
+        });
     });
 
     socket.on("edit-note", async (content: UpdateNoteDTO & { room: string }) => {
