809c43e67eb745878360f7f6cca13cff69be052f
Jessica Gondim <jessicagbsg@gmail.com>
2024-08-26 20:22:10 -0300

    feat: improve socket

--- src/socket.ts
+++ src/socket.ts
@@ -26,30 +26,46 @@ export const setUpSocket = (
   const nsp = io.of("/api/note");
 
   io.use((socket, next) => {
-    const token = socket.handshake.auth?.token || socket.handshake.headers.access_token?.toString();
-    if (!token) return "Authentication error";
-    const user = authenticationService.getUserFromToken(token);
-    if (!user) return "Authentication error";
-    return next();
+    try {
+      const token =
+        socket.handshake.auth?.token || socket.handshake.headers.access_token?.toString();
+      if (!token) {
+        console.error("Token missing");
+        return next(new Error("Authentication error"));
+      }
+
+      const user = authenticationService.getUserFromToken(token);
+      if (!user) {
+        console.error("Invalid token or user not found");
+        return next(new Error("Authentication error"));
+      }
+
+      socket.data.user = user;
+      next();
+    } catch (err) {
+      console.error("Error during authentication", err);
+      next(new Error("Authentication error"));
+    }
   });
 
   nsp.on("connection", (socket) => {
     console.log("New client connected");
+    const userRooms = new Map<string, Set<string>>();
 
-    const userRooms = new Map();
-
-    socket.on("join-room", async (content: { room: string }) => {
-      const user = await authenticationService.getUserFromToken(
-        socket.handshake.auth.token || socket.handshake.headers.access_token?.toString()
-      );
+    socket.on("join-room", async ({ room }) => {
+      const user =
+        socket.data.user ||
+        (await authenticationService.getUserFromToken(
+          socket.handshake.auth.token || socket.handshake.headers.access_token?.toString()
+        ));
       if (!user) return;
 
-      const note = await noteService.findByRoom(content.room, user.id);
-      if (!note) return nsp.to(note.room).emit("join-room", "Room not found");
+      const note = await noteService.findByRoom(room, user.id);
+      if (!note) return socket.emit("error", "Room not found");
 
-      const previousRooms = userRooms.get(user.id) || [];
-      for (const prevRoom of previousRooms) {
-        if (prevRoom !== content.room) {
+      const previousRooms = userRooms.get(user.id) || new Set();
+      previousRooms.forEach((prevRoom) => {
+        if (prevRoom !== room) {
           socket.leave(prevRoom);
           nsp.to(prevRoom).emit("leave-room", {
             user: user.id,
@@ -58,92 +74,103 @@ export const setUpSocket = (
             )} left collaboration`,
           });
         }
-      }
+      });
 
-      userRooms.set(user.id, [content.room]);
+      userRooms.set(user.id, new Set([room]));
 
       if (note.ownerId !== user.id && !note.members.includes(user.id)) {
         note.members.push(user.id);
         await noteService.update(note.id, { members: note.members });
       }
 
-      socket.join(content.room);
-      nsp.to(content.room).emit("join-room", {
+      socket.join(room);
+      nsp.to(room).emit("join-room", {
         user: user.id,
         message: `${capitalize(user.firstName)} ${capitalize(user.lastName)} joined collaboration`,
       });
     });
 
-    socket.on("leave-room", async (content: { room: string }) => {
-      const user = await authenticationService.getUserFromToken(
-        socket.handshake.auth.token || socket.handshake.headers.access_token?.toString()
-      );
+    socket.on("leave-room", async ({ room }) => {
+      const user =
+        socket.data.user ||
+        (await authenticationService.getUserFromToken(
+          socket.handshake.auth.token || socket.handshake.headers.access_token?.toString()
+        ));
       if (!user) return;
 
-      const note = await noteService.findByRoom(content.room, user.id);
-      if (!note) return nsp.to(content.room).emit("leave-room", "Room not found");
+      const note = await noteService.findByRoom(room, user.id);
+      if (!note) return socket.emit("error", "Room not found");
 
-      const userRoomList = userRooms.get(user.id) || [];
-      const updatedRoomList = userRoomList.filter((room: string) => room !== content.room);
-      userRooms.set(user.id, updatedRoomList);
+      const userRoomList = userRooms.get(user.id);
+      if (userRoomList) {
+        userRoomList.delete(room);
+        userRooms.set(user.id, userRoomList);
+      }
 
-      socket.leave(content.room);
-      nsp.to(content.room).emit("leave-room", {
+      socket.leave(room);
+      nsp.to(room).emit("leave-room", {
         user: user.id,
         message: `${capitalize(user.firstName)} ${capitalize(user.lastName)} left collaboration`,
       });
     });
 
-    socket.on("edit-note", async (content: UpdateNoteDTO & { room: string }) => {
-      const user = await authenticationService.getUserFromToken(
-        socket.handshake.auth.token || socket.handshake.headers.access_token?.toString()
-      );
+    socket.on(
+      "edit-note",
+      async ({ room, title, content, members }: UpdateNoteDTO & { room: string }) => {
+        const user =
+          socket.data.user ||
+          (await authenticationService.getUserFromToken(
+            socket.handshake.auth.token || socket.handshake.headers.access_token?.toString()
+          ));
+        if (!user) return;
+
+        const note = await noteService.findByRoom(room, user.id);
+        if (!note) return socket.emit("error", "Not able to find note");
+
+        if (note.ownerId !== user.id && !note.members.includes(user.id))
+          return socket.emit("error", "Not authorized");
+
+        await noteService.update(note.id, { title, content, members });
+
+        socket.join(room);
+        nsp.to(room).emit("edit-note", {
+          title,
+          content,
+          members,
+          updatedAt: Date.now(),
+        });
+      }
+    );
+
+    socket.on("delete-note", async ({ room }) => {
+      const user =
+        socket.data.user ||
+        (await authenticationService.getUserFromToken(
+          socket.handshake.auth.token || socket.handshake.headers.access_token?.toString()
+        ));
       if (!user) return;
 
-      const note = await noteService.findByRoom(content.room, user.id);
-      if (!note) return nsp.to(content.room).emit("edit-note", "Not able to find note");
+      const note = await noteService.findByRoom(room, user.id);
+      if (!note) return socket.emit("error", "Not able to find note");
 
       if (note.ownerId !== user.id && !note.members.includes(user.id))
-        return nsp.to(content.room).emit("delete-note", "Not authorized");
-
-      await noteService.update(note.id, {
-        title: content.title,
-        content: content.content,
-        members: content.members,
-      });
-
-      socket.join(content.room);
-      nsp.to(content.room).emit("edit-note", {
-        title: content.title,
-        content: content.content,
-        members: content.members,
-        updatedAt: Date.now(),
-      });
-    });
-
-    socket.on("delete-note", async (content: { room: string }) => {
-      const user = await authenticationService.getUserFromToken(
-        socket.handshake.auth.token || socket.handshake.headers.access_token?.toString()
-      );
-      if (!user) return;
-
-      const note = await noteService.findByRoom(content.room, user.id);
-
-      if (!note) return nsp.to(content.room).emit("delete-note", "Not able to find note");
-
-      if (note.ownerId !== user.id && !note.members.includes(user.id))
-        return nsp.to(content.room).emit("delete-note", "Not authorized");
+        return socket.emit("error", "Not authorized");
 
       const deleted = await noteService.delete(note.id, user.id);
 
-      socket.join(content.room);
-      nsp.to(content.room).emit("delete-note", deleted);
-      socket.leave(content.room);
+      nsp.to(room).emit("delete-note", deleted);
+      socket.leave(room);
       socket.disconnect();
     });
 
-    socket.on("disconnect", () => {
-      socket.disconnect();
+    socket.on("disconnect", async () => {
+      const user =
+        socket.data.user ||
+        (await authenticationService.getUserFromToken(
+          socket.handshake.auth.token || socket.handshake.headers.access_token?.toString()
+        ));
+      if (!user) return;
+      userRooms.delete(user.id);
       console.log("Client disconnected");
     });
   });
